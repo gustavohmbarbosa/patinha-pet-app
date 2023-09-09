4e9f9f0387e464fcd2dadd8938b0521ab6b48af2
Luanzacarias <luanzacarias215@gmail.com>
2023-09-09 17:55:45 -0300

    feat: layout page Trackers

--- src/screens/Trackers/index.tsx
+++ src/screens/Trackers/index.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 
 import { styles } from "./styles";
+import { FabIconBottom } from "../../components/FabIconBottom";
+import { CardTracker } from "../../components/CardTracker";
+import { TrackerProps } from "../../lib/props/TrackerProps";
+import { CardAlert } from "../../components/CardAlert";
 
 export default function Trackers() {
-  return <View style={styles.container}></View>;
+  var option: TrackerProps[] = [];
+  return (
+    <View style={styles.container}>
+      <FabIconBottom icon="plus" onPress={() => {}} />
+      <FlatList
+        data={option}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => {
+          return <CardTracker model={item.model} code={item.code} />;
+        }}
+        ListEmptyComponent={
+          <CardAlert text="Não há rastreadores vinculados a sua conta." />
+        }
+      />
+    </View>
+  );
 }
