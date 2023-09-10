3effa7ab1dbefb5db5dd80c248ba7ee6da1f19a8
Luanzacarias <luanzacarias215@gmail.com>
2023-09-10 11:53:27 -0300

    feat: add loading indicator map component

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -5,36 +5,43 @@ import { useMap } from "../../hooks/useMap";
 import { CardAlert } from "../CardAlert";
 import { Pin } from "../Pin";
 import { styles } from "./styles";
+import { Loading } from "../Loading";
 
 export function Map() {
-  const { positionUser } = useMap();
+  const { positionUser, isMapLoading } = useMap();
   return (
     <View style={styles.container}>
-      {positionUser ? (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: positionUser.coords.latitude,
-            longitude: positionUser.coords.longitude,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: positionUser.coords.latitude,
-              longitude: positionUser.coords.longitude,
-            }}
-            title="Você"
-            description="Sua localização atual"
-          >
-            <Pin isUser />
-          </Marker>
-        </MapView>
+      {isMapLoading ? (
+        <Loading />
       ) : (
-        <View style={styles.card}>
-          <CardAlert text="Permita o acesso de localização." />
-        </View>
+        <>
+          {positionUser ? (
+            <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: positionUser.coords.latitude,
+                longitude: positionUser.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }}
+            >
+              <Marker
+                coordinate={{
+                  latitude: positionUser.coords.latitude,
+                  longitude: positionUser.coords.longitude,
+                }}
+                title="Você"
+                description="Sua localização atual"
+              >
+                <Pin isUser />
+              </Marker>
+            </MapView>
+          ) : (
+            <View style={styles.card}>
+              <CardAlert text="Permita o acesso de localização." />
+            </View>
+          )}
+        </>
       )}
     </View>
   );
